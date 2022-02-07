a375c9610ca0342a3c5df5903cbb832c7eb8ca88
AlpSuuu <67225902+AlpSuuu@users.noreply.github.com>
2022-02-07 21:51:06 +0300

    Create test.js

---
+++ tests/test.js
@@ -0,0 +1,21 @@
+const Downloader = require("./src/index").default;
+const Util = new require("./src/index").Util;
+const Logger = Util.logger()
+
+
+let logger = new Logger(process);
+
+
+logger.oluştur("Bilgilerine bakmak istediğiniz medyanın linkini giriniz...\r\n" , cevap_1=> {
+    logger.oluştur("Url nin verisini mi göndermemi istersin medyasını mı? lütfen sadece `veri` ya da `medya` olarak cevap verin!!!\r\n" , cevap_2 => {
+        if(!["veri" , "medya"].some(x => cevap_2 === x)) return console.log("lütfen sadece `medya` ya da `veri` yazınız!!!")
+
+        let instagram = new Downloader(cevap_1.trim())
+
+        if(cevap_2.toLowerCase() === "veri") console.log("Belirttiğiniz Urlnin bilgileri gönderiliyor...\n") , console.log(instagram.getData);
+        if(cevap_2.toLowerCase() === "medya") console.log("Belirttiğiniz Urlnin Mediya bilgileri gönderiliyor...\n") , console.log(instagram.Media);
+
+        logger.kapat()
+    });
+})
+    
